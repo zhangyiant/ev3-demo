fcfd695c727580e47a7690bae21323ccbc858df2
Yi Zhang <zhangyiant@hotmail.com>
2021-07-04 16:12:04 +0800

    Add draw_text.

---
+++ draw_text/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+
+# Create your objects here.
+ev3 = EV3Brick()
+
+screen = ev3.screen
+
+screen.draw_text(30, 30, "HelloWorld!")
+
+wait(3000)
+
+screen.clear()
+
+screen.draw_text(30, 30, "VS Code!")
+
+wait(3000)
